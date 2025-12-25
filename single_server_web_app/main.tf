7edfde9ed466bb772994bd5b4ddf183d2cc3b200
Prince Kumar <kumarprince729781@gmail.com>
2025-12-25 15:40:30 +0530

    input output variable

--- single_server_web_app/main.tf
+++ single_server_web_app/main.tf
@@ -11,6 +11,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+// input variable for server port
+variable "server_port" {
+  description = "the port for ther server will use for HTTP request"
+  default = 8080
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0dee22c13ea7a9a67" // Ubuntu Server 24.04 LTS (HVM) - Free Tier eligible
     instance_type = "t3.micro"
@@ -19,7 +25,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World to AWS from Terraform" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p "${var.server_port}" &
                 EOF
 
     tags = {
@@ -31,10 +37,17 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
+// output the public IP address of the instance
+output "instance_ip_addr" {
+    description = "The public IP address of the web server instance"
+    value       = aws_instance.example.public_ip
 }
 
